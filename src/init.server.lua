4f5e872558a40c44968cde5c6ae760483bda677c
Dekkonot <dekkonot@users.noreply.github.com>
2020-02-03 15:09:31 -0800

    Add toolbar to toggle widget and plugin action to run serializer

--- src/init.server.lua
+++ src/init.server.lua
@@ -13,6 +13,8 @@ local pluginWarn = Util.pluginWarn
 local serializerInit, serialize = Serializer.init, Serializer.serialize
 local firstLoadConnection
 
+local firstLoadCompleted = false
+
 local serializePluginGui = plugin:CreateDockWidgetPluginGui("dekkonot-instance-serializer-main", DockWidgetPluginGuiInfo.new(
     Enum.InitialDockState.Float, --initDockState
     true, --initEnabled
@@ -25,8 +27,31 @@ serializePluginGui.Title = "Instance Serializer"
 
 UI.Background.Parent = serializePluginGui
 
+local function serializeSelected()
+    local currentSelection = Selection:Get()
+    if #currentSelection == 0 then
+        pluginWarn("An Instance must be selected to serialize!")
+    elseif #currentSelection > 1 then
+        for _, v in ipairs(currentSelection) do
+            local didSerialize, output = serialize(v)
+            if didSerialize then
+                output.Name = "Serialized_"..string.gsub(v.Name:gsub("[^%w_]+", ""), "^%d+", "")
+                output.Parent = v.Parent
+            end
+        end
+    else
+        local didSerialize, output = serialize(currentSelection[1])
+        if didSerialize then
+            Selection:Set({output})
+            -- plugin:OpenScript(output)
+            output.Parent = currentSelection[1].Parent
+        end
+    end
+end
+
 local function firstLoad()
     local success = serializerInit()
+    print('test', success)
     local settings = plugin:GetSetting("settings")
     if not settings then
         settings = {}
@@ -50,34 +75,19 @@ local function firstLoad()
     end)
     UIHandler()
     if success then
+        firstLoadCompleted = true
         UI.SerializeContainer.Visible = true
         UI.RetryContainer.Visible = false
         firstLoadConnection:Disconnect()
         UI.SerializeButton.InputBegan:Connect(function(input)
             if input.UserInputType ~= Enum.UserInputType.MouseButton1 then return end
-            local currentSelection = Selection:Get()
-            if #currentSelection == 0 then
-                pluginWarn("An Instance must be selected to serialize!")
-            elseif #currentSelection > 1 then
-                for _, v in ipairs(currentSelection) do
-                    local didSerialize, output = serialize(v)
-                    if didSerialize then
-                        output.Name = "Serialized_"..string.gsub(v.Name:gsub("[^%w_]+", ""), "^%d+", "")
-                        output.Parent = v.Parent
-                    end
-                end
-            else
-                local didSerialize, output = serialize(currentSelection[1])
-                if didSerialize then
-                    Selection:Set({output})
-                    -- plugin:OpenScript(output)
-                    output.Parent = currentSelection[1].Parent
-                end
-            end
+            serializeSelected()
         end)
+        return true
     else
         UI.SerializeContainer.Visible = false
         UI.RetryContainer.Visible = true
+        return false
     end
 end
 
@@ -90,3 +100,27 @@ firstLoadConnection = serializePluginGui:GetPropertyChangedSignal("Enabled"):Con
 if serializePluginGui.Enabled then
     firstLoad()
 end
+
+local toolbar = plugin:CreateToolbar("Instance Serializer")
+local toggleButton = toolbar:CreateButton("dekkonot-instance-serializer-toggle", "Toggle the serializer widget", "rbxassetid://2794885159", "Toggle Widget")
+local action = plugin:CreatePluginAction("dekkonot-instance-serializer-run", "Serialize Selected Instances", "Run the serializer with the current settings", "rbxassetid://2795131004", true)
+
+if serializePluginGui.Enabled then
+    toggleButton:SetActive(true)
+end
+
+toggleButton.Click:Connect(function()
+    serializePluginGui.Enabled = not serializePluginGui.Enabled
+    toggleButton:SetActive(serializePluginGui.Enabled)
+end)
+
+action.Triggered:Connect(function()
+    if firstLoadCompleted then
+        serializeSelected()
+    else
+        local success = firstLoad()
+        if success then
+            serializeSelected()
+        end
+    end
+end)
